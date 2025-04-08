ffdd6cfb44baf959bcecd9209a1354efa041206f
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-04-09 00:38:31 +0530

    updated .gitignore

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
