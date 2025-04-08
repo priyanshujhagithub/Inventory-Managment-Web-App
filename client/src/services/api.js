840574e62a35caf977d69196ea0c2070c203a23e
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-04-09 03:48:10 +0530

    enviroment variable changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 export const signUp = async (data) => {
     const response = await fetch(`${API_URL}/signup`,{
--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,6 @@
 import {io} from 'socket.io-client';
-const socket = io('http://localhost:5000', {
+const socketURL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000';
+const socket = io(socketURL, {
 
     path: '/socket.io',// default, but explicit helps
     transports: ['websocket','polling'], // try both transports
