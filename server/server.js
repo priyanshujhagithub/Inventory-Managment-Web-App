bf68c432df326332b7c5e893ac6d00227191504d
priyanshujhagithub <129042608+priyanshujhagithub@users.noreply.github.com.>
2025-04-13 21:02:09 +0530

    changes-1

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware
-app.use(cors());
+app.use(cors({origin:'https://inventory-managment-web-r1ubshfin.vercel.app'}));
 app.use(express.json());
 
 // Create HTTP server and setup Socket.IO
